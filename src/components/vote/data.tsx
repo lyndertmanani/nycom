2c4730355647052268c259e7b6958c2d0faabdfb
lyndertmanani <mananilyndert2@gmail.com>
2024-12-04 08:28:22 +0200

    data inserted

---
+++ src/components/vote/data.tsx
@@ -0,0 +1,49 @@
+export const categories = [
+    {
+      id: 1,
+      title: "TEAMWORK",
+      description: [
+        "Excels at working with individuals and teams.",
+        "Communicates well (talks good and listens too!).",
+        "Shares responsibilities and Kudos when the team succeeds.",
+      ],
+      question: "How does your nominee exemplify excellent teamwork?",
+    },
+    {
+      id: 2,
+      title: "MOST HARD WORKING",
+      description: [
+        "Understands our high-priority objectives and works hard to make them happen.",
+        "Shows top-notch ability to organize and manage small or large projects/groups.",
+        "Is a leader, individually, within any team or group...leads by word and by example.",
+      ],
+      question: "What very special tasks or projects have you seen your nominee do?",
+    },
+    {
+      id: 3,
+      title: "CUSTOMER SERVICE",
+      description: [
+        "Focuses on discovering, then meeting, fellow employees’ needs both on time and courteously.",
+        "Consistently sets the example of providing excellent internal Customer Service at the same level as external service.",
+        "Finds time to be involved in civic and voluntary work without complaining.",
+      ],
+      question: "In what ways is your nominee's customer service exemplary?",
+    },
+    {
+      id: 4,
+      title: "TIME-KEEPER",
+      description: [
+        "Regularly reports for work on time.",
+        "Delivers assignments on time and regularly beats deadlines for his/her assignments.",
+        "Cautious of time when attending and during meetings.",
+      ],
+      question: "In what ways is your nominee's time keeping exemplary?",
+    },
+    {
+      id: 5,
+      title: "WELL DRESSED",
+      description: ["Regularly dresses well for work and is always presentable."],
+      question: "In what ways is your nominee's dressing exemplary?",
+    },
+  ];
+  
