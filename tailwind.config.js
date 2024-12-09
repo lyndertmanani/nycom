2d7052ec94982338700feb5ad54116a5ae7b49fa
lyndertmanani <mananilyndert2@gmail.com>
2024-12-09 12:14:51 +0200

    vercel v2 debug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
