8cbb29f0732c9b3e30b829c57ab42f92e3f540c2
ericlyndert <mananilyndert2@gmail.com>
2024-12-13 15:21:00 +0200

    Update Nav.tsx

--- src/components/menu/Nav.tsx
+++ src/components/menu/Nav.tsx
@@ -44,8 +44,8 @@ export default function App() {
   return (
     <>
       {/* Navbar */}
-      <Navbar maxWidth="full" className="h-16 hidden p-5 bg-white/70">
-        <NavbarBrand>
+      <Navbar maxWidth="full" className="h-16     hidden p-5 bg-white/70">
+         <NavbarBrand>
           <NavLink to="/">
             <img src={Logo} className="rounded-none h-20 py-5" alt="Main logo" />
           </NavLink>
